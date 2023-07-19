6f3b6710d9d9ea364edb46e4d769ea8a5f4cfdf7
Odnanzd <fernando.trindade.ti@gmail.com>
2023-07-19 17:14:50 -0400

    19/07

--- app/src/main/java/com/example/tasksave/activity_main.java
+++ app/src/main/java/com/example/tasksave/activity_main.java
@@ -1,12 +1,14 @@
 package com.example.tasksave;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.ImageView;
 
 public class activity_main extends activity_login {
 
-
+public ImageView imageView;
 
     @SuppressLint({"MissingInflatedId", "SetTextI18n"})
     @Override
@@ -16,6 +18,17 @@ public class activity_main extends activity_login {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        imageView = findViewById(R.id.image_view_circle_agenda);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(activity_main.this, activity_agenda.class);
+                //intent2.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                //intent2.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent2);
+            }
+        });
     }
+}
