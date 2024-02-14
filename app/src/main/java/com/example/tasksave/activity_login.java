17f5cd27bbc9cfee2f0a747f058d43c5f65f80f0
Odnan <fernando.trindade.ti@gmail.com>
2024-02-13 22:57:57 -0300

    Firebase implementado.

--- app/src/main/java/com/example/tasksave/activity_login.java
+++ app/src/main/java/com/example/tasksave/activity_login.java
@@ -3,7 +3,6 @@ package com.example.tasksave;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.InputFilter;
@@ -11,18 +10,17 @@ import android.text.Spanned;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.Firebase;
 
 public class activity_login extends AppCompatActivity {
     public EditText input_Nome;
     public EditText input_Password;
     private Button button_login;
-
 
     @Override
     public void onBackPressed() {
