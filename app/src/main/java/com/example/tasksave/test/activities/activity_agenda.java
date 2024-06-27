177ccb72028a610afd4adfa8638ae9175bb9bd40
Odnanzd <fernando.trindade.ti@gmail.com>
2024-06-27 17:24:09 -0400

    Base teste versão 2.4
    iniciando filtros na activity_agenda
    
    att 5.

--- app/src/main/java/com/example/tasksave/test/activities/activity_agenda.java
+++ app/src/main/java/com/example/tasksave/test/activities/activity_agenda.java
@@ -43,6 +43,7 @@ import android.widget.Toast;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.tasksave.test.conexaoSQLite.Conexao;
@@ -79,6 +80,8 @@ public class activity_agenda extends AppCompatActivity implements CustomAdapter.
     ArrayList<Long> listaIDs = new ArrayList<>();
     ArrayList<Integer> repetirModoLembrete2 = new ArrayList<>();
 
+    CardView cardViewPendents;
+
     private SwipeRefreshLayout swipeRefreshLayout;
     @RequiresApi(api = Build.VERSION_CODES.O)
     @SuppressLint({"MissingInflatedId", "MissingSuperCall"})
@@ -104,6 +107,11 @@ public class activity_agenda extends AppCompatActivity implements CustomAdapter.
         swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout);
         imageViewFundoLista = findViewById(R.id.imageViewLista);
         imageViewBalao = findViewById(R.id.imageViewBalaoFala);
+
+        cardViewPendents = findViewById(R.id.cardViewPendentes);
+
+        final int originalColor = getResources().getColor(R.color.brancoGelo);
+        final int clickedColor = getResources().getColor(R.color.grey3);
 
         VerificaLista();
         ListarAgenda();
@@ -139,6 +147,28 @@ public class activity_agenda extends AppCompatActivity implements CustomAdapter.
 
             }
         });
+
+        cardViewPendents.setOnClickListener(new View.OnClickListener() {
+
+
+            private boolean isClicked = true;
+            @Override
+            public void onClick(View view) {
+
+
+                if (isClicked) {
+                    listarAgendaPendentes();
+                    cardViewPendents.setCardBackgroundColor(getResources().getColor(R.color.grey3));
+                } else {
+                    cardViewPendents.setCardBackgroundColor(getResources().getColor(R.color.brancoGelo));
+                    ListarAgenda();
+                }
+                isClicked = !isClicked;
+
+            }
+        });
+
+
 
     }
 
@@ -679,6 +709,109 @@ public class activity_agenda extends AppCompatActivity implements CustomAdapter.
             Log.d("TAREFA", "Millis: "+tarefa.getDateTimeInMillis());
 
         }
+
+    }
+
+    public void listarAgendaPendentes() {
+
+
+        List<Agenda> listaagenda = new ArrayList<Agenda>();
+        listaIDs.clear();
+        repetirModoLembrete2.clear();
+
+        Cursor cursor = db.rawQuery("SELECT * FROM agenda WHERE finalizado = 0 AND agendaAtraso = 1;", null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                @SuppressLint("Range")
+                long ID = cursor.getLong(cursor.getColumnIndex("id"));
+                @SuppressLint("Range")
+                String titulo = cursor.getString(cursor.getColumnIndex("nomeTarefa"));
+                @SuppressLint("Range")
+                String descricao = cursor.getString(cursor.getColumnIndex("descricaoTarefa"));
+                @SuppressLint("Range")
+                String dataagenda = cursor.getString(cursor.getColumnIndex("dataAgenda"));
+                @SuppressLint("Range")
+                int horaagenda = cursor.getInt(cursor.getColumnIndex("horaAgenda"));
+                @SuppressLint("Range")
+                int minutoagenda = cursor.getInt(cursor.getColumnIndex("minutoAgenda"));
+                @SuppressLint("Range")
+                int lembreteDB = cursor.getInt(cursor.getColumnIndex("lembretedefinido"));
+                boolean lembrete = (lembreteDB != 0);
+                @SuppressLint("Range")
+                int finalizadoDB = cursor.getInt(cursor.getColumnIndex("finalizado"));
+                boolean finalizado = (finalizadoDB != 0);
+                @SuppressLint("Range")
+                String dataagendaFim = cursor.getString(cursor.getColumnIndex("dataAgendaFim"));
+                @SuppressLint("Range")
+                int horaAgendaFim = cursor.getInt(cursor.getColumnIndex("horaAgendaFim"));
+                @SuppressLint("Range")
+                int minutoAgendaFim = cursor.getInt(cursor.getColumnIndex("minutoAgendaFim"));
+                @SuppressLint("Range")
+                String dataAgendaInsert = cursor.getString(cursor.getColumnIndex("dataAgendaInsert"));
+                @SuppressLint("Range")
+                int horaAgendaInsert = cursor.getInt(cursor.getColumnIndex("horaAgendaInsert"));
+                @SuppressLint("Range")
+                int minutoAgendaInsert = cursor.getInt(cursor.getColumnIndex("minutoAgendaInsert"));
+                @SuppressLint("Range")
+                int agendaAtrasoDB = cursor.getInt(cursor.getColumnIndex("agendaAtraso"));
+                boolean agendaAtraso = (agendaAtrasoDB != 0);
+                @SuppressLint("Range")
+                int repetirLembreteDB = cursor.getInt(cursor.getColumnIndex("repetirLembrete"));
+                boolean repetirLembrete = (repetirLembreteDB != 0);
+                @SuppressLint("Range")
+                int repetirLembreteModo = cursor.getInt(cursor.getColumnIndex("repetirModo"));
+                @SuppressLint("Range")
+                int notificouTarefaDB = cursor.getInt(cursor.getColumnIndex("notificouTarefa"));
+                boolean notificouTarefa = (notificouTarefaDB != 0);
+
+
+                @SuppressLint({"NewApi", "LocalSuppress"}) LocalDate localdataagenda = LocalDate.parse(dataagenda, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+                @SuppressLint({"NewApi", "LocalSuppress"}) LocalDate localdataagendaFim = LocalDate.parse(dataagendaFim, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+                @SuppressLint({"NewApi", "LocalSuppress"}) LocalDate localdataagendaInsert = LocalDate.parse(dataAgendaInsert, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+                listaagenda.add(new Agenda(ID, titulo, descricao, localdataagenda, horaagenda, minutoagenda,
+                        lembrete, finalizado, localdataagendaFim, horaAgendaFim, minutoAgendaFim, localdataagendaInsert,
+                        horaAgendaInsert, minutoAgendaInsert, agendaAtraso, repetirLembrete, repetirLembreteModo, notificouTarefa));
+                listaIDs.add(ID);
+                repetirModoLembrete2.add(repetirLembreteModo);
+
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+
+        // Convertendo a lista de objetos Agenda em arrays separados para o CustomAdapter
+        String[] titulos = new String[listaagenda.size()];
+        String[] descricoes = new String[listaagenda.size()];
+        String[] datas = new String[listaagenda.size()];
+        String[] horas = new String[listaagenda.size()];
+        boolean[] lembretes = new boolean[listaagenda.size()];
+        long[] ids = new long[listaagenda.size()];
+        boolean[] repetirLembrete = new boolean[listaagenda.size()];
+        int[] repetirModoLembrete = new int[listaagenda.size()];
+        boolean[] notificouTarefa = new boolean[listaagenda.size()];
+
+        for (int i = 0; i < listaagenda.size(); i++) {
+            ids[i] = listaIDs.get(i);
+            titulos[i] = listaagenda.get(i).getNomeAgenda();
+            descricoes[i] = listaagenda.get(i).getDescriçãoAgenda();
+            datas[i] = listaagenda.get(i).getDataAgendaString();
+            int hora = listaagenda.get(i).getHoraAgenda();
+            int minuto = listaagenda.get(i).getMinutoAgenda();
+            String horaFormatada = String.format(Locale.getDefault(), "%02d:%02d", hora, minuto);
+            horas[i] = horaFormatada;
+            lembretes[i] = listaagenda.get(i).getLembrete();
+            repetirLembrete[i] = listaagenda.get(i).getRepetirLembrete();
+            repetirModoLembrete[i] = repetirModoLembrete2.get(i);
+            notificouTarefa[i] = listaagenda.get(i).isNotificado();
+        }
+
+        // Configurando o CustomAdapter para a ListView
+        CustomAdapter customAdapter = new CustomAdapter(activity_agenda.this, listaIDs, titulos, descricoes, datas, horas,
+                lembretes, repetirLembrete, repetirModoLembrete2, notificouTarefa);
+        listView.setAdapter(customAdapter);
+
 
     }
 
